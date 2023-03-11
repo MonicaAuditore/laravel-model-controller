2b51a2bdf9b7cb9af9ca8c80c2d447674dbb9798
MonicaAuditore <mauditore@live.it>
2023-03-11 11:40:46 +0100

    add_creazione database

---
+++ app/Http/Controllers/Guest/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+//Models
+use App\Models\Movie;
+
+class MainController extends Controller
+{
+    public function index() {
+        $movies = Movie::all();
+
+dd($movies);
+
+        return view('welcome');
+    }
+    
+}
