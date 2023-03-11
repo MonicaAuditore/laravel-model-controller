9e4940fdb2a7617e80b1ec3ca0f988339c05f3c0
MonicaAuditore <mauditore@live.it>
2023-03-11 12:25:36 +0100

    add_Stampa card in pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,15 +8,23 @@
         @vite('resources/js/app.js')
     </head>
     <body>
-        <img src="{{ Vite::asset('resources/img/logo.png') }}" alt=""> 
+        {{-- <img src="{{ Vite::asset('resources/img/logo.png') }}" alt="">  --}}
         <main>
-            <div class=row>
-                <div class="col">
+            <div class="container">
+                <div class="row row-cols-1 row-cols-lg-5 row-cols-md-3 row-cols-sm-1 g-4 pt-5 pb-5">
                     @foreach ($movies as $movie)
-                        <ul>
-                            <li> {{$movie->id}} </li>
-                            <li> {{$movie->title}} </li>
-                        </ul>
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">{{$movie->title}}</h5>
+                                <p class="card-text text-center">Titolo originale: {{$movie->original_title}}</p>
+                                <p class="card-text mb-0">Nazionalità: {{$movie->nationality}}</p>
+                                <p class="card-text mb-0">Data: {{$movie->date}}</p>
+                                <span class="card-text">Voto:</span><span><strong> {{$movie->vote}}</strong></span>
+                            </div>
+                        </div>
+                    </div>
                     @endforeach
                 </div>
             </div>
